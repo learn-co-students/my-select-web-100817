77a5aa278acec35dba9250038f57f260be1d4692
flored27 <dflores@fandm.edu>
2017-09-27 19:21:31 -0400

    tricky

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ newcollection = []
+ while i < collection.length
+   yield collection[i]
+   yesorno = yield collection[i]
+   if yesorno == true
+     newcollection.push(collection[i])
+   end
+   i+=1
+ end
+   return newcollection
 end
