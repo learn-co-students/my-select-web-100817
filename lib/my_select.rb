9819be4981b006f86b97d09b538aa107f426ec5a
tohanian <tadehohanian@gmail.com>
2017-10-02 22:04:59 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    new_array = []
+    counter = 0
+    while counter < collection.length
+      if yield(collection[counter])
+        new_array << collection[counter]
+      end
+      counter += 1
+    end
+    return new_array
+  else
+    return nil
+  end
 end
